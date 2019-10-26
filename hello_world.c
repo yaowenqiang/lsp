efdc6aad2b7dcc992641a1622be2d87aba76b46a
Yaowenqiang <yaowenqiang111@163.com>
2019-10-26 13:58:11 +0800

    add error handling demo

--- hello_world.c
+++ hello_world.c
@@ -1,9 +1,23 @@
 #include <fcntl.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main()
 {
     int fd;
+    /* 
     fd = open("foo", O_WRONLY | O_CREAT, 8644);
+    if (fd < 0) {
+        printf("Error number %d\n", errno);
+        perror("foo");
+        exit(1);
+    } */
+    if ((fd = open("foo", O_WRONLY | O_CREAT, 0644)) < 0) {
+        printf("Error number %d\n", errno);
+        perror("foo");
+        exit(1);
+    }
     write(fd, "Hello world", 11);
     close(fd);
 
