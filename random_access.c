4cf857b95dfde64d1d7011c851854d7491c252a7
Yaowenqiang <yaowenqiang111@163.com>
2019-10-27 17:29:57 +0800

    add random_access file  via lseek demo

---
+++ random_access.c
@@ -0,0 +1,22 @@
+#include <unistd.h>
+#include <fcntl.h>
+
+struct  record {
+    int id;
+    char name[80];
+
+};
+
+int main(void)
+{
+    int fd, size = sizeof(struct record);
+    struct record info;
+    fd = open("datafile", O_RDWR);
+    lseek(fd, size, SEEK_SET);
+    read(fd, &info, size);
+    info.id = 99;
+    lseek(fd, -size, SEEK_CUR);
+    write(fd, &info, size);
+    close(fd);
+    return 0;
+}
