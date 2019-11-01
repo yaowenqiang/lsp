4bd98258176fba8d59f51863a1e920c6858d27f7
Yaowenqiang <yaowenqiang111@163.com>
2019-11-01 21:48:37 +0800

    add perror strerror assert demo

--- tmpfile.c
+++ tmpfile.c
@@ -1,13 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <error.h>
+#include <errno.h>
+#include <string.h>
+#include <assert.h>
 #define BSIZE 10
 
 int main(void)
 {
     
-    FILE * tmp = tmpfile();
-    //FILE * tmp = fopen("tmp.txt", "r+");
+    //FILE * tmp = tmpfile();
+    FILE * tmp = fopen("tmp.txt", "r+");
     int read_count, write_count, i;
 
     //char * output = malloc(sizeof(char));
@@ -25,7 +28,13 @@ int main(void)
 
     //fclose(tmp);
 
-    //tmp = fopen("tmp.txt", "r+");
+    tmp = fopen("tmp1.txt", "r+");
+    assert(tmp != NULL);
+    if (tmp == NULL ) {
+        perror(strerror(errno));
+        fprintf(stderr, "error reading tmp file %s\n: ", strerror(errno));
+        exit(EXIT_FAILURE);
+    }
     //tmp = tmpfile();
     fflush(tmp);
     read_count = fread(output,1, 1, tmp);
