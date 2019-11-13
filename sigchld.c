3274249ae4dcb1561ab6db5830c8e5defea2d386
Yaowenqiang <yaowenqiang111@163.com>
2019-11-13 22:00:23 +0800

    add SIGCHLD signal handler demo

---
+++ sigchld.c
@@ -0,0 +1,26 @@
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+sig_atomic_t child_exit_status;
+
+void clean_up_child_process(int signal_number)
+{
+    int status;
+    wait(&status);
+    child_exit_status = status;
+
+}
+
+
+int main(void)
+{
+
+    struct sigaction sigchild_action;
+    memset(&sigchild_action, 0, sizeof(sigchild_action));
+    sigchild_action.sa_handler = &clean_up_child_process;
+    sigaction(SIGCHLD, &sigchild_action, NULL);
+
+    return 0;
+}
