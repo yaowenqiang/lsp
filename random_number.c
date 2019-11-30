344f3bf4801c54991c16b4bb4c2ca8a3c53e9bfc
Yaowenqiang <yaowenqiang111@163.com>
2019-11-30 22:40:10 +0800

    add random_number demo viar /dev/random device

---
+++ random_number.c
@@ -0,0 +1,42 @@
+#include <assert.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+int random_number(int min, int max)
+{
+    static int dev_random_fd = -1;
+    char* next_random_byte;
+    int bytes_to_read;
+    unsigned random_value;
+
+    assert(max > min);
+    if (dev_random_fd == -1) {
+        dev_random_fd = open("/dev/random", O_RDONLY);
+        assert(dev_random_fd != -1);
+    }
+
+    next_random_byte = (char*)&random_value;
+    bytes_to_read = sizeof(random_value);
+
+    do {
+        int bytes_read;
+        bytes_read = read(dev_random_fd, next_random_byte, bytes_to_read);
+        bytes_to_read -= bytes_read;
+        next_random_byte += bytes_read;
+
+    } while (bytes_to_read > 0);
+
+    return min + (random_value % (max - min + 1));
+
+}
+
+void main(void) {
+    int min = 1;
+    int max = 100;
+
+    int rand = random_number(min, max);
+    printf("%d\n", rand);
+}
+
